d05c8ab68f8f516f2f726c28e4a49a5ddc881956
oceanosis <dumluufuk@gmail.com>
2020-08-29 20:58:06 +0100

    fix cidr

--- vpc.tf
+++ vpc.tf
@@ -43,7 +43,7 @@ resource "aws_subnet" "main-public-2" {
 }
 resource "aws_subnet" "main-public-3" {
     vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.2.0/24"
+    cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = var.availability_zone_names[2]
 
