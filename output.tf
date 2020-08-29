85333094c47453d2c41842c0693d8093bbf10e8c
oceanosis <dumluufuk@gmail.com>
2020-08-29 19:29:05 +0100

    outputing subnet ids

--- output.tf
+++ output.tf
@@ -2,3 +2,10 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+output "public_subnet_id" {
+  value = data.aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  value = data.aws_subnet.private.id
+}
