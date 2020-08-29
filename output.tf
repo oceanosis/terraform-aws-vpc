471fda855ed2d96e06e07a72fde63713afc1ad1f
oceanosis <dumluufuk@gmail.com>
2020-08-29 19:36:57 +0100

    remove ids from output

--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-
-output "public_subnet_id" {
-  value = data.aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = data.aws_subnet.private.id
-}
